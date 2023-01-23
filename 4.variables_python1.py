5723ba31fd3e012d202e38a80084861e1bd61759
siva3010 <sivag30102001@gmail.com>
2023-01-23 22:37:22 +0530

    variable

--- 4.variables_python1.py
+++ 4.variables_python1.py
@@ -6,3 +6,29 @@ my_tax = my_income * tax_raete
 print(my_tax)
 
 print(type(my_tax))
+
+#variable name
+
+my_name="siva"
+
+print(my_name)
+#variable can not contain stating number,white space,-
+
+#assigning multiple variable
+x,y,z=10,10,10
+print(x,y,z)
+
+#Global variable
+
+x1="siva"
+
+def myname():
+    print(x1+"prakash")
+myname()    
+
+x2="siva"
+def my():
+    global x
+    x="prakash"
+my()
+print("x"+x2)    
