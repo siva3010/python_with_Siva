b3cb2c6751765f49e9ed905380d873d296c1abcf
siva3010 <sivag30102001@gmail.com>
2023-01-23 21:53:34 +0530

    two created

---
+++ Data_Structure.py
@@ -0,0 +1,39 @@
+int=10
+#int is Such as integer like whole nubers 10 30 40 50
+
+float=3.0
+#float is such as decimal point like 3.0 4.0
+
+str="Hello world"
+#str stands for string like "name" 'siva'
+
+list=[1,"siva",30,40]
+#ordered sequence of Object 
+# List can be created by []
+
+dict={"siva":10,"nme":'surya'}
+#Unorder keys:valve pars  
+#can be created using{:}
+
+tuple=("siva",1,2,"siva")
+#ordered immutuble sequence of object
+#tupule is same as list
+#tupule can be created using ()
+
+set={"a","b"}
+#unorder collection of unique object
+#can be created using {}
+
+bool=True or False
+#True or false
+#Logical values
+
+
+
+print(int)
+print(float)
+print(set)
+print(dict)
+print(bool)
+print(tuple)
+print(list)
